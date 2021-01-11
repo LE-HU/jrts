92b7734847449c769d9b0c3b7d5decf4ec3f8aa7
LE-HU <wojciechlech1337@gmail.com>
2021-01-12 00:22:27 +0100

    add available_tickets to Event, add Gateway Payment Adapter, seed the database

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
   has_many :tickets
   has_many :users, through: :appointments
+
+  def available_tickets
+    guest_limit - tickets_count
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  User.create!(email: "example#{n + 1}@example.com",
+               password: "example#{n + 1}@example.com")
+end
+
+20.times do
+  Event.create!(name: Faker::Game.title,
+                description: Faker::TvShows::BojackHorseman.tongue_twister,
+                event_time: Time.now + rand(1..1_000_000),
+                guest_limit: rand(50..150),
+                ticket_price: rand(1000..10_000))
+end
+
+30.times do
+  Ticket.create!(user_id: rand(1..10),
+                 event_id: rand(1..20),
+                 paid: [true, false].sample)
+end
+
+puts 'Seed successful.'
