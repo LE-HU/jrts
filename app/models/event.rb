65ada9dbfcc1820da414bf29ac9d11048f876fa1
LE-HU <wojciechlech1337@gmail.com>
2021-01-13 02:18:24 +0100

    fix event association typo, redesign event#show to show detailed info

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   has_many :tickets, dependent: :destroy
   has_many :paid_tickets, -> { paid }, class_name: 'Ticket'
-  has_many :users, through: :appointments
+  has_many :users, through: :tickets
 
   validates :name, presence: true, length: { maximum: 80 }
   validates :description, length: { maximum: 3000 }
@@ -10,11 +10,23 @@ class Event < ApplicationRecord
   validates :ticket_price, presence: true, numericality: { only_integer: true }
   validate :event_happens_in_the_future
 
-  def available_tickets
-    guest_limit - paid_tickets.size
+  def detailed_info
+    users_with_ticket = users.each_with_object([]) do |user, arr|
+      arr << { user: user.email }
+    end
+
+    { name: name,
+      description: description,
+      date: event_time,
+      ticket_price: ticket_price,
+      participants: users_with_ticket }.to_json
   end
 
   private
+
+  def available_tickets
+    guest_limit - paid_tickets.size
+  end
 
   def event_happens_in_the_future
     if event_time.present? && event_time < Time.now
